777f3c44423e8e0af43b5705a8a3c6a2a951e4a6
matheuswanderley <matheuswanderley1988@gmail.com>
2024-04-05 12:26:46 -0300

    feat: add constants

--- src/middlewares/middlewareJwt.js
+++ src/middlewares/middlewareJwt.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
 const { validateClaims } = require('../utils/validateClaims');
+const { validMessagesWhenTokenIsInvalidOrMissing, HTTP_STATUS_FORBIDDEN, HTTP_STATUS_OK, HTTP_STATUS_UNAUTHORIZED } = require('../utils/constants')
 
-const verifyJWT = (req, res, next) => {
+const verifyJWT = (req, res) => {
     const token = req.query.token;
     if (!token) {
-        return res.status(403).json({ success: false, message: "Token is missing." });
+        return res.status(HTTP_STATUS_FORBIDDEN).json({ success: false, message: validMessagesWhenTokenIsInvalidOrMissing.token_missing });
     }
 
     try {
         const decodedPayload = jwt.decode(token);
         const isValid = validateClaims(decodedPayload);
-        return res.status(200).json({ success: true, isValid });
+        return res.status(HTTP_STATUS_OK).json({ success: true, isValid });
     } catch (error) {
-        return res.status(401).json({ success: false, message: "Invalid Token", error: error });
+        return res.status(HTTP_STATUS_UNAUTHORIZED).json({ success: false, message: validMessagesWhenTokenIsInvalidOrMissing.token_invalid, error: error });
     }
 };
 
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+const validMessagesWhenTokenIsInvalidOrMissing = { 'token_missing': 'Token is missing.', 'token_invalid': 'Invalid Token' }
+const HTTP_STATUS_FORBIDDEN = 403
+const HTTP_STATUS_OK = 200
+const HTTP_STATUS_UNAUTHORIZED = 401
+
+module.exports = { validMessagesWhenTokenIsInvalidOrMissing, HTTP_STATUS_FORBIDDEN, HTTP_STATUS_OK, HTTP_STATUS_UNAUTHORIZED }
