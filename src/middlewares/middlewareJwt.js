351fab559879b2c2fda91ddda687058efbad7698
matheuswanderley <matheuswanderley1988@gmail.com>
2024-04-09 08:27:44 -0300

    refac: forgot next() function, and change logic in the middleware and simplify the route

--- src/middlewares/middlewareJwt.js
+++ src/middlewares/middlewareJwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { validateClaims } = require('../utils/validateClaims');
 const { validMessagesWhenTokenIsInvalidOrMissing, HTTP_STATUS_FORBIDDEN, HTTP_STATUS_OK, HTTP_STATUS_UNAUTHORIZED } = require('../utils/constants')
 
-const verifyJWT = (req, res) => {
+const verifyJWT = (req, res, next) => {
     const token = req.query.token;
     if (!token) {
         return res.status(HTTP_STATUS_FORBIDDEN).json({ success: false, message: validMessagesWhenTokenIsInvalidOrMissing.token_missing });
@@ -11,7 +11,8 @@ const verifyJWT = (req, res) => {
     try {
         const decodedPayload = jwt.decode(token);
         const isValid = validateClaims(decodedPayload);
-        return res.status(HTTP_STATUS_OK).json({ success: true, isValid });
+        req.userIsValid = isValid;
+        next();
     } catch (error) {
         return res.status(HTTP_STATUS_UNAUTHORIZED).json({ success: false, message: validMessagesWhenTokenIsInvalidOrMissing.token_invalid, error: error });
     }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { validateClaims } = require('../utils/validateClaims');
 const { verifyJWT } = require('../middlewares/middlewareJwt');
 
 
 router.get('/validate-jwt', verifyJWT, (req, res) => {
-    const claims = req.user;
-    const isValid = validateClaims(claims);
-    res.json({ success: isValid });
+    res.json({ success: req.userIsValid });
 });
 
 module.exports = router;
